de8191e345cbebefb90ea89ddc675a75fb793d1b
Thomas Deuling <info@coding.ms>
2013-09-08 18:08:59 +0200

    Add: - Fluid-If for iterations within f:for - Categories - Keyboard handling optimization

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -20,16 +20,19 @@
 snippet t(:fluidNamespace) do |snip|
   snip.trigger = "{namespace"
   snip.expansion = "{namespace ${1:name} = ${1:viewHelper}}"
+  snip.category = "Fluid Inline"
 end
  
 snippet t(:fluidLayout) do |snip|
   snip.trigger = "<f:layout"
-  snip.expansion = "<f:layout name=\"${1:name}\"></f:layout>${2}"
+  snip.expansion = "<f:layout name=\"${1:name}\"></f:layout>$0"
+  snip.category = "Fluid Tag"
 end
  
 snippet t(:fluidSection) do |snip|
   snip.trigger = "<f:section"
-  snip.expansion = "<f:section name=\"${1:name}\">${2}</f:section>"
+  snip.expansion = "<f:section name=\"${1:name}\">$0</f:section>"
+  snip.category = "Fluid Tag"
 end
  
  
@@ -38,15 +41,18 @@ end
 #####################################
 snippet t(:fluidRenderSection) do |snip|
   snip.trigger = "<f:render"
-  snip.expansion = "<f:render section=\"${1:sectionname}\"></f:render>${2}"
+  snip.expansion = "<f:render section=\"${1:sectionname}\"></f:render>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidRenderPartial) do |snip|
   snip.trigger = "<f:render"
-  snip.expansion = "<f:render partial=\"${1:sectionname}\"></f:render>${2}"
+  snip.expansion = "<f:render partial=\"${1:sectionname}\"></f:render>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidRenderPartialWithArguments) do |snip|
   snip.trigger = "<f:render"
-  snip.expansion = "<f:render partial=\"${1:sectionname}\" arguments=\"{${2:name}:${3:variable}}\"></f:render>${4}"
+  snip.expansion = "<f:render partial=\"${1:sectionname}\" arguments=\"{${2:name}:${3:variable}}\"></f:render>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -55,12 +61,14 @@ end
 #####################################
 snippet t(:fluidFlashMessages) do |snip|
   snip.trigger = "<f:flashMessages"
-  snip.expansion = "<f:flashMessages></f:flashMessages>${2}"
+  snip.expansion = "<f:flashMessages></f:flashMessages>$0"
+  snip.category = "Fluid Tag"
 end
 
 snippet t(:fluidFlashMessagesInDiv) do |snip|
   snip.trigger = "<f:flashMessages"
-  snip.expansion = "<f:flashMessages renderMode=\"div\"></f:flashMessages>${2}"
+  snip.expansion = "<f:flashMessages renderMode=\"div\"></f:flashMessages>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -69,12 +77,14 @@ end
 #####################################
 snippet t(:fluidTranslate) do |snip|
   snip.trigger = "<f:translate"
-  snip.expansion = "<f:translate key=\"${1:key}\"></f:translate>${2}"
+  snip.expansion = "<f:translate key=\"${1:key}\"></f:translate>$0"
+  snip.category = "Fluid Tag"
 end
 
 snippet t(:fluidTranslateWithArguments) do |snip|
   snip.trigger = "<f:translate"
-  snip.expansion = "<f:translate key=\"${1:key}\" arguments=\"{0: ${2:var}}\"></f:translate>${3}"
+  snip.expansion = "<f:translate key=\"${1:key}\" arguments=\"{0: ${2:var}}\"></f:translate>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -84,17 +94,44 @@ end
 #####################################
 snippet t(:fluidIf) do |snip|
   snip.trigger = "<f:if"
-  snip.expansion = "<f:if condition=\"{${1:condition}}\"></f:if>${2}"
+  snip.expansion = "<f:if condition=\"{${1:condition}}\">$0</f:if>"
+  snip.category = "Fluid Tag"
 end
-
 snippet t(:fluidThen) do |snip|
   snip.trigger = "<f:then"
-  snip.expansion = "<f:then></f:then>${1}"
+  snip.expansion = "<f:then>$0</f:then>"
+  snip.category = "Fluid Tag"
 end
-
 snippet t(:fluidElse) do |snip|
   snip.trigger = "<f:else"
-  snip.expansion = "<f:else></f:else>${1}"
+  snip.expansion = "<f:else>$0</f:else>"
+  snip.category = "Fluid Tag"
+end
+
+snippet t(:fluidIfIterationIsOdd) do |snip|
+  snip.trigger = "<f:if"
+  snip.expansion = "<f:if condition=\"{itemIteration.isOdd}\">$0</f:if>"
+  snip.category = "Fluid Tag"
+end
+snippet t(:fluidIfIterationIsEven) do |snip|
+  snip.trigger = "<f:if"
+  snip.expansion = "<f:if condition=\"{itemIteration.isEven}\">$0</f:if>"
+  snip.category = "Fluid Tag"
+end
+snippet t(:fluidIfIterationIsFirst) do |snip|
+  snip.trigger = "<f:if"
+  snip.expansion = "<f:if condition=\"{itemIteration.isFirst}\">$0</f:if>"
+  snip.category = "Fluid Tag"
+end
+snippet t(:fluidIfIterationIsLast) do |snip|
+  snip.trigger = "<f:if"
+  snip.expansion = "<f:if condition=\"{itemIteration.isLast}\">$0</f:if>"
+  snip.category = "Fluid Tag"
+end
+snippet t(:fluidIfIterationModulo) do |snip|
+  snip.trigger = "<f:if"
+  snip.expansion = "<f:if condition=\"{itemIteration.cycle} % ${1:number}\">$0</f:if>"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -104,7 +141,8 @@ end
 #####################################
 snippet t(:fluidImage) do |snip|
   snip.trigger = "<f:image"
-  snip.expansion = "<f:image src=\"${1:src}\" alt=\"{${1:alt}}\"></f:image>${3}"
+  snip.expansion = "<f:image src=\"${1:src}\" alt=\"{${1:alt}}\"></f:image>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -116,21 +154,25 @@ end
 #####################################
 snippet t(:fluidCount) do |snip|
   snip.trigger = "<f:count"
-  snip.expansion = "<f:count subject=\"{${1:myarray}}\"></f:count>${2}"
+  snip.expansion = "<f:count subject=\"{${1:myarray}}\"></f:count>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidCountInline) do |snip|
   snip.trigger = "{f:count"
-  snip.expansion = "{f:count(${1:myarray})} > 0}${2}"
+  snip.expansion = "{f:count(${1:myarray})} > 0}$0"
+  snip.category = "Fluid Inline"
 end
 
 snippet t(:fluidDebug) do |snip|
   snip.trigger = "<f:debug"
-  snip.expansion = "<f:debug title=\"${1:title}\">{${2:object}}</f:debug>"
+  snip.expansion = "<f:debug title=\"${1:title}\">{${2:object}}</f:debug>$0"
+  snip.category = "Fluid Tag"
 end
 
 snippet t(:fluidComment) do |snip|
   snip.trigger = "<f:comment"
-  snip.expansion = "<f:comment>${1:comment}</f:comment>"
+  snip.expansion = "<f:comment>${1:comment}</f:comment>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -140,15 +182,18 @@ end
 #####################################
 snippet t(:fluidFor) do |snip|
   snip.trigger = "<f:for"
-  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\">\n</f:for>"
+  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\">$0\n</f:for>"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidForWithIteration) do |snip|
   snip.trigger = "<f:for"
-  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\" iteration=\"${4:itemIteration}\">\n</f:for>"
+  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\" iteration=\"${4:itemIteration}\">$0\n</f:for>"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidForWithIterationReverse) do |snip|
   snip.trigger = "<f:for"
-  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\" iteration=\"${4:itemIteration}\" reverse=\"1\">\n</f:for>"
+  snip.expansion = "<f:for each=\"{${1:array}}\" as=\"${2:value}\" key=\"${3:value}\" iteration=\"${4:itemIteration}\" reverse=\"1\">$0\n</f:for>"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -158,15 +203,18 @@ end
 #####################################
 snippet t(:fluidLinkAction) do |snip|
   snip.trigger = "<f:link.action"
-  snip.expansion = "<f:link.action action=\"${1:action}\">${2:linktext}</f:link.action>"
+  snip.expansion = "<f:link.action action=\"${1:action}\">${2:linktext}</f:link.action>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidLinkActionWithController) do |snip|
   snip.trigger = "<f:link.action"
-  snip.expansion = "<f:link.action action=\"${1:action}\" controller=\"${2:controller}\">${3:linktext}</f:link.action>"
+  snip.expansion = "<f:link.action action=\"${1:action}\" controller=\"${2:controller}\">${3:linktext}</f:link.action>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidLinkActionWithControllerAndArguments) do |snip|
   snip.trigger = "<f:link.action"
-  snip.expansion = "<f:link.action action=\"${1:action}\" controller=\"${2:controller}\" alt=\"{${3:name}:${4:variable}}\">${5:linktext}</f:link.action>"
+  snip.expansion = "<f:link.action action=\"${1:action}\" controller=\"${2:controller}\" alt=\"{${3:name}:${4:variable}}\">${5:linktext}</f:link.action>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -176,15 +224,18 @@ end
 #####################################
 snippet t(:fluidLinkPage) do |snip|
   snip.trigger = "<f:link.page"
-  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\">${2:linktext}</f:link.page>"
+  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\">${2:linktext}</f:link.page>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidLinkPageWithAdditionalParams) do |snip|
   snip.trigger = "<f:link.page"
-  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\" additionalParams=\"{${2:name}:${3:variable}}\">${4:linktext}</f:link.page>"
+  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\" additionalParams=\"{${2:name}:${3:variable}}\">${4:linktext}</f:link.page>$0"
+  snip.category = "Fluid Tag"
 end
 snippet t(:fluidLinkPageWithPageType) do |snip|
   snip.trigger = "<f:link.page"
-  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\" pageType=\"${2:pageType}\">${5:linktext}</f:link.page>"
+  snip.expansion = "<f:link.page pageUid=\"${1:pageUid}\" pageType=\"${2:pageType}\">${5:linktext}</f:link.page>$0"
+  snip.category = "Fluid Tag"
 end
 
 
@@ -192,13 +243,22 @@ end
 
 # Fluid-Snippets
 #####################################
-snippet "Fluid flashMessages template" do |snip|
+snippet t(:fluidTemplateFlashMessage) do |snip|
   snip.trigger = "f:flashMessages"
   snip.expansion  = "\t<div class=\"tx-coding-ms-base-flashMessages flashMessages\">\n"
   snip.expansion += "\t\t<f:flashMessages></f:flashMessages>\n"
-  snip.expansion += "\t</div>\n"
+  snip.expansion += "\t</div>\n$0"
+  snip.category = "Fluid Code-Snippets"
 end
 
+snippet t(:fluidTemplateIfIterationModulo) do |snip|
+  snip.trigger = "f:if"
+  snip.expansion  = "\t<f:if condition=\"{itemIteration.cycle} % ${1:number}\">\n"
+  snip.expansion += "\t\t<f:then><!-- when its not modulo --></f:then>\n"
+  snip.expansion += "\t\t<f:else>$0</f:else>\n"
+  snip.expansion += "\t</f:if>\n"
+  snip.category = "Fluid Code-Snippets"
+end
 
 
 
